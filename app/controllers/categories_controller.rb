680a8388ee6398cea0205db7747fb299f5a6b91e
blanklist <joehineriv@gmail.com>
2017-06-18 19:11:41 -0700

    new, show, index

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new    
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    @category.save
+    redirect_to @category
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:title)
+    end
+
+end
